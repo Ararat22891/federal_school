6b5b7020f7d3025d31253a01b27086387a410c33
name <tanusaakovleva07@gmail.com>
2024-02-28 22:11:58 +0300

    myEdit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,19 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
 
+import 'dart:math';
 import 'package:agora_uikit/agora_uikit.dart';
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:federal_school/data/push/notificationController.dart';
 import 'package:federal_school/data/push/pushData.dart';
-import 'package:federal_school/domain/models/user/user.dart';
-import 'package:federal_school/domain/states/login/loginViewModel.dart';
 import 'package:federal_school/presentation/pages/home/homeView.dart';
 import 'package:federal_school/presentation/pages/login/loginView.dart';
 import 'package:federal_school/presentation/themes/themes.dart';
-import 'package:federal_school/testView.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:intl/date_symbol_data_local.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 User? _user;
